2f2d777fcc26c437b4a24a765be82d98567492dd
Yanhui Shen <shen.elf@gmail.com>
2012-02-25 20:05:13 +0800

    +Store(), Restore(), List()

--- trash.py
+++ trash.py
@@ -5,10 +5,11 @@ import os
 import uuid
 import time
 import shutil
+import string
 from shenpy import *
 
 trash_max = 1024
-trash_dir = os.path.expanduser('~/') + '.mytrash/'
+trash_dir = os.path.expanduser('~/') + 'MyTrash' + '/'
 item_content = 'content'
 item_desc = 'desc'
 desc_date = 'date '
@@ -18,19 +19,20 @@ desc_compressed = 'compressed '
 
 # Print help.
 def TrashHelp():
-    info = '''Usage:
-	-h	    Show this message.
-	-s	    Show trash status.
-	-l	    List all items in trash.
-	-a	    Store items to its previous path.
-	-r [1 2 n]  Restore items to its previous path.
-	-z [1 2 n]  Compress items in trash.
-	-Z [1 2 n]  Uncomprress items in trash.
-	-d [1 2 n]  Delete items from trash.'''
-    print(info)
+	table = [['Usage:'],\
+			['', '-h', 'Show this message.'],\
+			['', '-s', 'Show trash status.'],\
+			['', '-l', 'List all items in trash.'],\
+			['', '-a', 'Store items to trash.'],\
+			['', '-r [1 2 n] -t [path]', 'Restore items.'],\
+			['', '-z [1 2 n]', 'Compress items in trash.'],\
+			['', '-Z [1 2 n]', 'Uncomprress items in trash.'],\
+			['', '-d [1 2 n]', 'Delete items from trash.']]
+
+	ColPrint(table)
 
 # Init trash directory.
-def trash_init():
+def TrashInit():
 	if os.path.isdir(trash_dir) and os.access(trash_dir, os.W_OK):
 		return True
 
@@ -46,18 +48,24 @@ def trash_init():
 # Show trash status.
 def TrashStatus():
 	size = str(FileSize(trash_dir))
-	print('Trash path:\t' + trash_dir)
-	print('Trash size:\t' + HRSize(size))
+	table = [['Trash path:', trash_dir],\
+			['Trash size:', HRSize(size)]]
+	ColPrint(table)
+
+	TrashList()
 
 # List items in trash.
 def TrashList():
-	table = [['NO.', 'Size', 'Date', 'Z', 'From']]
+	table = [['NO.', 'Name', 'Size', 'Date', 'Z', 'From']]
 
 	items = os.listdir(trash_dir)
 	for idx, item in enumerate(items):
-		cols = []
+		itemDir = trash_dir + item + '/'
+		contentDir = itemDir + item_content + '/'
 
-		descPath = trash_dir + item + '/desc'
+		name = os.listdir(contentDir)[0]
+
+		descPath = itemDir + item_desc
 		desc = open(descPath, mode='r', encoding='utf-8')
 
 		line = desc.readline().strip('\n')
@@ -72,21 +80,17 @@ def TrashList():
 
 		line = desc.readline().strip('\n')
 		compressed = line[line.find(' ')+1:]
-		if bool(compressed):
-			compressed = 'N'
-		else:
+		if compressed.lower() == 'true':
 			compressed = 'Y'
+		else:
+			compressed = 'N'
 
 		desc.close()
 
-		cols.append(str(idx+1))
-		cols.append(size)
-		cols.append(date)
-		cols.append(compressed)
-		cols.append(oldPath)
+		cols = [str(idx+1), name, size, date, compressed, oldPath]
 		table.append(cols)
 
-	ColPrint(table, 2, [1, 2, 1, 1, 1])
+	ColPrint(table, 2, [1, 1, 2, 1, 1, 1])
 
 # Delete items from trash.
 def TrashDelete():
@@ -94,9 +98,9 @@ def TrashDelete():
 
 # Recycle the given item into trash.
 def TrashStore(argvs):
-	cwd = os.getcwd() + '/';
 	for argv in argvs:
-		oldPath = cwd + argv
+		oldPath = os.path.abspath(argv)
+		oldDir = os.path.dirname(oldPath)
 		newDir = trash_dir + uuid.uuid1().hex + '/'
 		contentDir = newDir + item_content
 		
@@ -120,7 +124,7 @@ def TrashStore(argvs):
 		desc = open(newDir + item_desc, mode='w', encoding='utf-8')
 		desc.write(desc_date + date + '\n')
 		desc.write(desc_size + size + '\n')
-		desc.write(desc_path + oldPath + '\n')
+		desc.write(desc_path + oldDir + '\n')
 		desc.write(desc_compressed + 'false\n')
 		desc.close()
 
@@ -128,17 +132,119 @@ def TrashStore(argvs):
 
 		print(MsgColor.OkGreen + 'size:' + MsgColor.Endc, HRSize(size))
 
-# Restore items to its previous path.
-def TrashRestore():
-    desc = open()
-    pass
+# Restore items.
+def TrashRestore(argvs):
+	items = os.listdir(trash_dir)
+
+	index = []
+	toPath = ''
+	for idx, strIdx in enumerate(argvs):
+		if strIdx == '-t' and len(argvs) > idx+1:
+			toPath = argvs[idx+1]
+			break
+
+		if strIdx.isdigit() and int(strIdx) <= len(items) and int(strIdx) > 0:
+			index.append(int(strIdx)-1)
+			continue
+	index.sort()
+	if len(index) <= 0:
+		return
+
+	print(os.path.abspath(toPath))
+
+	table = [['NO.', 'Name', 'Size', 'Date', 'Z', 'From', 'To']]
+	for idx, item in enumerate(items):
+		if idx not in index:
+			continue
+
+		itemDir = trash_dir + item + '/'
+		contentDir = itemDir + item_content + '/'
+		descPath = itemDir + item_desc
+
+		name = os.listdir(contentDir)[0]
+
+		desc = open(descPath, mode='r', encoding='utf-8')
+
+		line = desc.readline().strip('\n')
+		date = line[line.find(' ')+1:]
+
+		line = desc.readline().strip('\n')
+		size = line[line.find(' ')+1:]
+		size = HRSize(size)
+
+		line = desc.readline().strip('\n')
+		oldPath = line[line.find(' ')+1:]
+
+		line = desc.readline().strip('\n')
+		compressed = line[line.find(' ')+1:]
+		if compressed.lower() == 'true':
+			compressed = 'Y'
+		else:
+			compressed = 'N'
+
+		desc.close()
+
+		destDir = oldPath + '/'
+		if len(toPath) != 0:
+			destDir= os.path.abspath(toPath) + '/'
+
+		cols = [str(idx+1), name, size, date, compressed, oldPath, destDir]
+		table.append(cols)
+
+	ColPrint(table, 2, [1, 1, 2, 1, 1, 1, 1])
+
+	accept = input('Accept? [y/n] ')
+	if accept.lower() not in ['y', 'yes']:
+		return
+
+	for idx, item in enumerate(items):
+		if idx not in index:
+			continue
+
+		itemDir = trash_dir + item + '/'
+		contentDir = itemDir + item_content + '/'
+		descPath = itemDir + item_desc
+
+		name = os.listdir(contentDir)[0]
+
+		desc = open(descPath, mode='r', encoding='utf-8')
+
+		line = desc.readline().strip('\n')
+		line = desc.readline().strip('\n')
+		line = desc.readline().strip('\n')
+		oldPath = line[line.find(' ')+1:]
+
+		line = desc.readline().strip('\n')
+		#compressed = line[line.find(' ')+1:]
+		#if compressed.lower() == 'true':
+
+		desc.close()
+
+		destDir = oldPath + '/'
+		if len(toPath) != 0:
+			destDir= os.path.abspath(toPath) + '/'
+		if (os.path.isdir(destDir) and not os.access(destDir, os.W_OK)) or \
+				os.path.isfile(destDir):
+			print(MsgColor.Fail + "Invaild destination!", MsgColor.Endc)
+			continue
+		if os.path.exists(destDir+name):
+			print(MsgColor.Fail + "File already exists!", MsgColor.Endc)
+			continue
+
+		if not os.path.exists(destDir):
+			os.makedirs(destDir)
+
+		shutil.move(contentDir+name, destDir+name)
+		os.rmdir(contentDir)
+		os.remove(descPath)
+		os.rmdir(itemDir)
 
 # Compress items from trash.
 def TrashCompress():
     pass
 
 # Main Function
-if not trash_init():
+if not TrashInit():
 	print(MsgColor.Fail + 'Could not initialize trash!' + MsgColor.Endc)
 	sys.exit(-1)
 
